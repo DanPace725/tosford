cb1ab6238e632b3cef54a9a991facdd86b0a17bc
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2023-02-08 16:39:36 -0800

    upates

---
+++ functions.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import openai
+import json
+
+# Get Response Function -----
+def get_response(prompt):
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    if response:
+        return response["choices"][0]["text"]
+    else:
+        return None
+
+# Get Prompt Function -----
+def get_prompt(prompt_id):
+    with open('prompts.json') as f:
+        prompts = json.load(f)
+    return prompts.get(prompt_id)
