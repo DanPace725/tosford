11ce169b1dc376cfb7f023784e1ba7a7f1a02802
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2023-02-11 10:49:23 -0800

    Update main.py

--- custom_functions.py
+++ custom_functions.py
@@ -10,15 +10,19 @@ def get_response(prompt):
         model="text-davinci-003",
         prompt=prompt,
         temperature=0.9,
-        max_tokens=4097,
+        max_tokens=2000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
-        
+      
     )
-
+    
     if response:
-        return response["choices"][0]["text"]
+        return {
+        "text": response["choices"][0]["text"],
+        "total_tokens": response["usage"]["total_tokens"]
+        }
+
     else:
         return "There was a problem"
 
